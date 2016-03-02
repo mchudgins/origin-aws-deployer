85b9d235a8dcdfe92b86913ecaa6e1df0c4c7ccd
Mike Hudgins <mchudgins@dstsystems.com>
2016-03-02 15:32:34 -0600

    working on launch script

--- bake-master-image.sh
+++ bake-master-image.sh
@@ -18,16 +18,19 @@ cat <<"EOF" > ${BOOT_FILE}
 #! /bin/bash
 
 DEFAULT_OPENSHIFT_DOWNLOAD=https://github.com/openshift/origin/releases/download/v1.1.3/openshift-origin-server-v1.1.3-cffae05-linux-64bit.tar.gz
+
 TARFILE=ose.tar.gz
 PUBLIC_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4/`
 INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
 AMI_ID=`curl http://169.254.169.254/latest/meta-data/ami-id`
+REGION=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[abcdefg]$//'`
 
 if [[ -z "${OPENSHIFT_DOWNLOAD}" ]]; then
   OPENSHIFT_DOWNLOAD=${DEFAULT_OPENSHIFT_DOWNLOAD}
 fi
 
-# some magic voodoo to parse the download string and pluck out the /vX.X.X/
+# some magic voodoo to parse the download string and pluck out the
+# the version number from /vX.X.X/ in the URL
 RELEASE=`echo ${OPENSHIFT_DOWNLOAD} | sed 's/^.*\/v//' | sed 's/\/.*//'`
 
 echo "Using ${OPENSHIFT_DOWNLOAD} as the source for Openshift Origin, v${RELEASE}."
@@ -46,20 +49,90 @@ dnf clean all
 #sudo systemctl enable rc-local.service
 
 # download & setup the openshift runtimes
+mkdir -p /opt/origin
+cd /opt/origin
+mkdir bin
 curl -sL ${OPENSHIFT_DOWNLOAD} -o /tmp/${TARFILE} \
   && tar xvfz /tmp/${TARFILE} --strip-components=1 \
+    --directory bin \
   && rm /tmp/${TARFILE}
+
+#-----------------------------------------------------------
+# Create initial-setup.sh
+#-----------------------------------------------------------
+
+cat <<"EOF_SETUP" >bin/initial-setup.sh
+#! /bin/bash
+
+MASTER_IP=192.168.1.10
+DEFAULT_MASTER_DNS=dev.dstcorp.io
+DEFAULT_CLUSTER_UPLOAD=s3://dstresearch/cluster-configs
+REGION=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[abcdefg]$//'`
+
+if [[ -e /tmp/launch-config ]]; then
+  source /tmp/launch-config
+fi
+
+if [[ -z "${MASTER_DNS}" ]]; then
+  MASTER_DNS=${DEFAULT_MASTER_DNS}
+fi
+
+if [[ -z "${CLUSTER_UPLOAD}" ]]; then
+  CLUSTER_UPLOAD=${DEFAULT_CLUSTER_UPLOAD}
+fi
+
+
+# create the master certificates and config
+
+/opt/origin/bin/openshift start master \
+  --listen=https://${MASTER_IP}:8443 \
+  --master=https://${MASTER_IP}:8443 \
+  --public-master=https://${MASTER_DNS}:8443 \
+  --network-plugin=redhat/openshift-ovs-multitenant \
+  --write-config=openshift.local.config/master
+
+# now that the CA has been created (by the start master cmd),
+# we need to create the node config's and certificates
+# we'll create twenty of 'em for ip addresses 192.168.1.20 thru .39
+
+for i in `seq 20 39`; do
+  NODE=192-168-1-$i;
+  NODEIP=192.168.1.$i;
+  /opt/origin/bin/oadm create-node-config \
+    --node-dir=openshift.local.config/ip-${NODE} \
+    --node=ip-${NODE}.ec2.internal \
+    --hostnames=ip-${NODE}.ec2.internal,${NODEIP} \
+    --network-plugin=redhat/openshift-ovs-multitenant \
+    --master=https://${MASTER_IP}:8443;
+
+  # tweak the mtu settings.  AWS supports an mtu of 9000
+  sed -i 's/mtu: *1450/mtu: 8950/' openshift.local.config/ip-${NODE}/node-config.yaml
+done
+
+# copy the configs up to s3
+tar cvfz /tmp/config.tar.gz openshift.local.config \
+  && aws s3 --region=${REGION} cp \
+    /tmp/config.tar.gz ${CLUSTER_UPLOAD}/${MASTER_DNS}/config.tar.gz \
+  && rm /tmp/config.tar.gz
+
+# now setup the local machine's config as a master
+mkdir -p /etc/origin/openshift.local.config/master
+#mkdir -p /etc/origin/openshift.local.config/`hostname -s`
+cp -ra openshift.local.config/master/* /etc/origin/openshift.local.config/master
+#cp -ra openshift.local.config/`hostname -s` /etc/origin/openshift.local.config/`hostname -s`
+rm -rf openshift.local.config
+EOF_SETUP
+chmod +x bin/initial-setup.sh
 
 # now, create the image
 TODAY=`date +%Y%m%d`
 BASE_OS=`cat /etc/redhat-release`
 UNAME=`uname -rvmpio`
-REGION=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[abcdefg]$//'`
 AMI_DESC=`aws ec2 --region=${REGION} describe-images --image-ids ami-1033037a | jq '.Images[0].Name'`
 
 # copy the /var/log/cloud-init-output.log to s3, so we can track what happened.
 echo "/var/log/cloud-init-output.log uploaded to s3://dstresearch/logs/Openshift-v${RELEASE}-Master-${TODAY}-${INSTANCE_ID}.log"
-echo "Requesting AWS to create image:  Openshift-v${RELEASE}-Master-${TODAY}"
+echo "Requesting AWS create image:  Openshift-v${RELEASE}-Master-${TODAY}"
 aws s3 cp --region=${REGION} /var/log/cloud-init-output.log \
   s3://dstresearch/logs/Openshift-v${RELEASE}-Master-${TODAY}-${INSTANCE_ID}.log
 
